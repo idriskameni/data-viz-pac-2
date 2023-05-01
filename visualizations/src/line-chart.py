9529f6e86d5f1e3043bdf14012cbd2cec56db34a
Adrián Alonso Gonzalo <adrian.alonsogonzalo@gmail.com>
2023-05-01 14:21:40 +0200

    created line-chart

---
+++ visualizations/src/line-chart.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('../data/line-chart.csv')
+
+df = df.loc[df['NOM_ENS'] == 'Ajuntament de Ripollet']
+df = df.drop(['CODI_10', 'NOM_ENS', 'HOMES', 'DONES'], axis=1)
+df = df.sort_values('ANY')
+
+fig, ax = plt.subplots()
+
+ax.plot(df['ANY'], df['TOTAL'])
+
+ax.set_title('Habitants de Ripollet per any')
+ax.set_xlabel('Any')
+ax.set_ylabel('Habitants')
+
+ax.text(0.5, -0.25, 'Font: Dades històriques de població \ndels municipis de Catalunya (Dades obertes Catalunya)', ha='center', va='bottom', transform=plt.gca().transAxes)
+
+plt.savefig('../images/line-chart.jpg', dpi=300, bbox_inches='tight')
