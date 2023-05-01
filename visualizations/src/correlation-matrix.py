01c84830fa9be091d5c320bb3753cc167e5d8a38
Adrián Alonso Gonzalo <adrian.alonsogonzalo@gmail.com>
2023-05-01 19:51:19 +0200

    changed correlation matrix graph

--- visualizations/src/correlation-matrix.py
+++ visualizations/src/correlation-matrix.py
@@ -1,24 +1,24 @@
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('../data/correlation-matrix.csv', delimiter=';')
+df = pd.read_csv('../data/correlation-matrix.data')
+df.columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
 
-corr_matrix = df.corr()
-
-print(corr_matrix)
+corr = df.iloc[:, :4].corr()
 
 fig, ax = plt.subplots()
 
-im = ax.imshow(corr_matrix, cmap='coolwarm')
-ax.figure.colorbar(im, ax=ax)
+sns.heatmap(corr, annot=True, cmap='coolwarm', square=True, 
+            xticklabels=corr.columns.values, yticklabels=corr.columns.values,
+            annot_kws={'fontsize':12},
+            ax=ax
+)
 
-ax.set_title('Habitants de Ripollet per any')
-ax.set_xlabel('Any')
-ax.set_ylabel('Habitants')
-
-ax.set_title("Correlation Matrix")
+ax.set_title('Correlation Matrix del joc de dades \'Iris\'')
 
 plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+plt.yticks(rotation=0)
 
 fig.set_size_inches(8, 4)
 plt.savefig('../images/correlation-matrix.jpg', dpi=300, bbox_inches='tight')
