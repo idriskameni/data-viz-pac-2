10d46aba1bbc47df20a8def2102ff09a06eb6cb7
Adrián Alonso Gonzalo <adrian.alonsogonzalo@gmail.com>
2023-05-01 20:12:03 +0200

    added source to ohlc

--- visualizations/src/ohlc-chart.py
+++ visualizations/src/ohlc-chart.py
@@ -15,7 +15,7 @@ ax.set_title('OHLC Chart del preus de les accions d\'Amazon l\'Abril del 2023')
 ax.set_ylabel('Dates')
 ax.set_ylabel('Preu')
 
-ax.text(0.5, -0.25, 'Font: Yahoo Finance - \nAmazon.com, Inc. (AMZN)', ha='center', va='bottom', transform=plt.gca().transAxes)
+ax.text(0.5, -0.3, 'Font: Yahoo Finance - Amazon.com, Inc. (AMZN)', ha='center', va='bottom', transform=plt.gca().transAxes)
 
 fig.set_size_inches(8, 4)
 plt.savefig('../images/ohlc-chart.jpg', dpi=300, bbox_inches='tight')
